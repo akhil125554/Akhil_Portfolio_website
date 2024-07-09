c3139c121f95a2c3de4138ce6cbaa9358907f155
akhil125554 <126895797+akhil125554@users.noreply.github.com>
2024-07-09 21:23:01 +0530

    vs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
 );
